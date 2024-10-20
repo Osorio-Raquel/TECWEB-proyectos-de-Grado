80e1be966fa6bb03d4efa7c9a41f7d21b98bf9dc
Raquel Osorio <142281150+Osorio-Raquel@users.noreply.github.com>
2024-10-20 10:22:36 -0400

    MAVEN UPDATE

---
+++ demo1/src/test/java/com/example/demo1/Demo1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.demo1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = Demo1Application.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
